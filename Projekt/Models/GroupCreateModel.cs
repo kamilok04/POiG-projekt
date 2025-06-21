77002db8b18719f1d91076b79aa3c571492af61f
Jakub Wizner <kuba.wizner@gmail.com>
2025-06-21 15:45:46 +0200

    Podstawy kreatora grup

---
+++ Projekt/Models/GroupCreateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projekt.Miscellaneous;
+
+namespace Projekt.Models
+{
+    public class GroupCreateModel(LoginWrapper loginWrapper) : BaseTableModel(loginWrapper), ITable
+    {
+        public string TableName => "groups";
+        public string? DefaultQuery => String.Empty; // No default query for creation
+        public Dictionary<string, object>? DefaultParameters => null;
+    }
+}
